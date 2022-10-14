61c34d3cfcbfabfcbd285d7303f4714e971229fd
gfouz <gfouz1975@gmail.com>
2022-10-14 05:45:05 -0400

    another commit

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ const express = require("express")
 const PORT = process.env.PORT || 5000;
 const routes = require("./routes/routes");
 const app = express();
-const cors = require('cors');
-app.use(cors());
+
 app.use(express.static("public"));
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => res.render('index'));
 
 
