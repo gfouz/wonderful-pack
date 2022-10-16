915127ded0080a4c30053efd35340decaf6aef4e
gfouz <gfouz1975@gmail.com>
2022-10-16 04:01:27 -0400

    continue to fix routes

--- routes/routes.js
+++ routes/routes.js
@@ -10,18 +10,56 @@ const {
   getProvinces
 } = require("../controllers/provincia");
 
+const {
+  createMunicipio,
+  getMunicipioById,
+  getMunicipiosEnabled,
+  getMunicipios,
+  getMunicipiosByProvincia,
+  getMunicipiosByProvinciaEnabled,
+  updateMunicipio,
+} = require("../controllers/municipio");
+
+const {
+  createUser,
+  getUsers,
+  updateUser,
+  loginUser,
+  getUserById,
+  getUsersEnabled,
+  getUsersByRol,
+} = require("../controllers/user");
+
 
 // provinces routes
 router.get("/provincias/:id", getProvinceById);
-router.get("/enabled", getProvinciasEnabled);
+router.get("/provincias/enabled/:enabled", getProvinciasEnabled);
 router.put("/provincias/:id", updateProvincia);
-router.get("/name", getProvinciasByName);
+router.get("/provincias/name/:name", getProvinciasByName);
 router.get("/provincias", getProvinces);
 router.post("/provincias", createProvince);
 
 
+// Municipio Routes
+router.get("/municipios/:id", getMunicipioById);
+router.get("/municipios/provincia/:id", getMunicipiosByProvincia);
+router.get(
+  "/municipios/provincia/enabled/:enabled",
+  getMunicipiosByProvinciaEnabled
+);
+router.get("/municipios/enabled/:enabled", getMunicipiosEnabled);
+router.post("/municipios", createMunicipio);
+router.put("/municipios/:id", updateMunicipio);
+router.get("/municipios", getMunicipios);
 
-
+// User Routes
+router.post("/users", createUser);
+router.post("/login", loginUser);
+router.get("/users/:id", getUserById);
+router.put("/users/:id", updateUser);
+router.get("/users/enabled/:enabled", getUsersEnabled);
+router.get("/users/rol/:id", getUsersByRol);
+router.get("/users", getUsers);
 
 
 module.exports = router;
